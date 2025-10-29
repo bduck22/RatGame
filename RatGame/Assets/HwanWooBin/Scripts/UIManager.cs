fdcbfe87165b9a23e0b0ec1bf5667ff250c7a6ce
bduck22 <hwangwb1224@hanmail.net>
2025-10-29 17:00:21 +0900

    1029-1700
    +약초방 UI

--- RatGame/Assets/HwanWooBin/Scripts/UIManager.cs
+++ RatGame/Assets/HwanWooBin/Scripts/UIManager.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Animator Fade;
+
+    public Animator[] UIs;
+
+    public Transform touchVfx;
+
     void Start()
     {
-        
+        Fade = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonUp(0))
+        {
+            touchVfx.position = Input.mousePosition;
+            touchVfx.GetComponent<Animator>().SetTrigger("Play");
+            //touchVfx.GetComponent<ParticleSystem>().Play();
+        }
+#endif
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                touchVfx.position = touch.position;
+                touchVfx.GetComponent<Animator>().SetTrigger("Play");
+                //touchVfx.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+
+    public void CloseUi()
+    {
+
+        foreach ( Animator ani in UIs)
+        {
+            ani.SetTrigger("Play");
+        }
+    }
+
+    public void OnUi()
+    {
+        foreach ( Animator ani in UIs)
+        {
+            ani.SetTrigger("Back");
+        }
+    }
+
+    public void OnFade()
+    {
+        Fade.SetTrigger("Play");
     }
 }
